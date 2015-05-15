3bbec1040c7b3ba5abea6f1907d50a21949e8ff6
hunnitgz-Aziz <sonicthehamilton.ar@gmail.com>
2015-05-15 18:36:43 -0400

    clocking out

--- js/init.js
+++ js/init.js
@@ -5,3 +5,28 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+(function ($) {
+  $(document).ready(function(){
+    
+  // hide .navbar first
+  $(".navi-skrollz").hide();
+  
+  // fade in .navbar
+  $(function () {
+    $(window).scroll(function () {
+            // set distance user needs to scroll before we fadeIn navbar
+      if ($(this).scrollTop() > 100) {
+        $('.navi-skrollz').fadeIn();
+        $('.origi-nav').fadeOut();
+      } else {
+        $('.navi-skrollz').fadeOut();
+        $('.origi-nav').fadeIn();
+      }
+    });
+
+  
+  });
+
+});
+  }(jQuery));
